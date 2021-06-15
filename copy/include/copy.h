5a3250e303c2b2d760ae58d4e623313b36a3a86c
Rafael Silva Freire <rafaelsilvafreire2@gamil.com>
2021-06-15 20:14:12 -0300

    Feito copy

---
+++ copy/include/copy.h
@@ -0,0 +1,28 @@
+#ifndef GRAAL_H
+#define GRAAL_H
+
+#include <utility>
+using std::pair;
+#include <iterator>
+using std::distance;
+#include <algorithm>
+using std::sort;
+
+namespace graal {
+
+/*! 
+ * TODO: documentação no estilo doxygen
+ */
+template<class InputIt>
+InputIt copy(InputIt first, InputIt last, InputIt d_first)
+{
+    for(auto it=first; it!=last; it++) {
+        *d_first = *it;
+        d_first++;
+    }
+
+    return d_first;
+}
+
+}
+#endif
