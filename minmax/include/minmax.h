69366d8d9cc0336302f4e0c3206cd9d3675d5871
Rafael Silva Freire <rafaelsilvafreire2@gamil.com>
2021-06-15 19:34:15 -0300

    Feito minmax

---
+++ minmax/include/minmax.h
@@ -0,0 +1,46 @@
+#ifndef GRAAL_H
+#define GRAAL_H
+
+#include <utility>
+using std::pair;
+#include <iterator>
+using std::distance;
+#include <algorithm>
+using std::sort;
+
+namespace graal {
+
+/*! 
+ * Exemplo de documentação seguindo o formato Doxygen
+ *
+ * @tparam Itr iterator para o range.
+ * @tparam Compare o tipo de uma função bool(const T &a, const T &b)
+ *
+ * @param first Ponteiro para o primeiro elemento do range
+ * @param last Ponteiro para a posição logo após o último elemento do range
+ * @param cmp A função de comparação que retorna true quando o primeiro parâmetro é menor do que o segundo
+ *
+ * @return Um std::pair contendo o menor e maior elemento, nesta ordem
+ *
+ */
+template <typename Itr, typename Compare >
+std::pair<Itr, Itr> minmax( Itr first, Itr last, Compare cmp )
+{
+    Itr smaller = first;
+    Itr bigger = first;
+
+    for(Itr it=first; it!=last; it++) {
+        if(cmp(*it, *smaller)) {
+            smaller = it;
+        }
+
+        if(*bigger <= *it) {
+            bigger = it;
+        }
+    }
+    
+    return std::make_pair( smaller, bigger );
+}
+
+}
+#endif
