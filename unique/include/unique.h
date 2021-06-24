c5a5af74db3010d89d75612c27ff03a9db531786
Rafael Silva Freire <rafaelsilvafreire2@gamil.com>
2021-06-24 10:51:44 -0300

    Adicionado domcumentaçao em unique

--- unique/include/unique.h
+++ unique/include/unique.h
@@ -11,6 +11,18 @@ using std::sort;
 
 namespace graal {
 
+/*! 
+ *
+ * @tparam InputIt iterator para o range.
+ * @tparam Equal o tipo de uma função bool(const T &a, const T &b)
+ *
+ * @param v Vetor de ponteiros iterators
+ * @param it Ponteiro que sera procurado no vector
+ * @param eq A função de comparação que retorna true quando os 2 elementos são iguais
+ * 
+ * @return true caso ache o ponteiro iterator no vetor
+ *
+ */
 template<class InputIt, class Equal>
 bool search(vector<InputIt>& v, InputIt it, Equal eq) {
     for(InputIt value : v) {
@@ -24,7 +36,17 @@ bool search(vector<InputIt>& v, InputIt it, Equal eq) {
 
 
 /*! 
- * TODO: documentação no estilo doxygen
+ * Exemplo de documentação seguindo o formato Doxygen
+ *
+ * @tparam InputIt iterator para o range.
+ * @tparam Equal o tipo de uma função bool(const T &a, const T &b)
+ *
+ * @param first Ponteiro para o primeiro elemento do range
+ * @param last Ponteiro para a posição logo após o último elemento do range
+ * @param eq A função de comparação que retorna true quando os 2 elementos são iguais
+ * 
+ * @return a posição logo após a última do range após a remoção dos elementos iguais
+ *
  */
 template<class InputIt, class Equal>
 InputIt unique( InputIt first, InputIt last, Equal eq )
