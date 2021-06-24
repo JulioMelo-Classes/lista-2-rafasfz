cb798a1253a8addeff4c9cc9510b65c5566c8d97
Rafael Silva Freire <rafaelsilvafreire2@gamil.com>
2021-06-24 10:44:43 -0300

    Adicionado domcumentaçao em equal

--- equal/include/equal.h
+++ equal/include/equal.h
@@ -11,7 +11,17 @@ using std::sort;
 namespace graal {
 
 /*! 
- * TODO: documentação no estilo doxygen
+ *
+ * @tparam Itr iterator para o range.
+ * @tparam Equal o tipo de uma função bool(const T &a, const T &b)
+ *
+ * @param first1 Ponteiro para o primeiro elemento do primeiro range
+ * @param last1 Ponteiro para a posição logo após o último elemento do primeiro range
+ * @param first2 Ponteiro para o primeiro elemento do segundo range
+ * @param eq A função de comparação que retorna true quandoos elmentos são iguais
+ * 
+ * @return true se os ranges foram iguais
+ *
  */
 template<class InputIt1, class InputIt2, class Equal>
 bool equal(InputIt1 first1, InputIt1 last1, InputIt2 first2, Equal eq)
@@ -28,6 +38,20 @@ bool equal(InputIt1 first1, InputIt1 last1, InputIt2 first2, Equal eq)
     return true;
 }
 
+/*! 
+ *
+ * @tparam Itr iterator para o range.
+ * @tparam Equal o tipo de uma função bool(const T &a, const T &b)
+ *
+ * @param first1 Ponteiro para o primeiro elemento do primeiro range
+ * @param last1 Ponteiro para a posição logo após o último elemento do primeiro range
+ * @param first2 Ponteiro para o primeiro elemento do segundo range
+ * @param last2 Ponteiro para a posição logo após o último elmento do segundo range
+ * @param eq A função de comparação que retorna true quandoos elmentos são iguais
+ * 
+ * @return true se os ranges foram iguais
+ *
+ */
 template<class InputIt1, class InputIt2, class Equal>
 bool equal(InputIt1 first1, InputIt1 last1,
            InputIt2 first2, InputIt2 last2,
